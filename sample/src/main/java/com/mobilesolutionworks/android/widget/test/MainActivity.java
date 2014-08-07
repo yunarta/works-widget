73d97107293173a7f9b0426bc217bb9c0c389ac4
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2014-08-08 02:22:30 +0800

    Demonstratation of Works Widget

---
+++ sample/src/main/java/com/mobilesolutionworks/android/widget/test/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mobilesolutionworks.android.widget.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by yunarta on 8/8/14.
+ * ${applicationId}
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
